347b9d17176ae299b2c9c80151211a0c1f711f2f
ximenamzo <ximenamzo@users.noreply.github.com>
2024-09-05 15:42:36 -0600

    LTS: a3 y a5 + index

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.html',
+    './public/**/*.js',
+    './*.html',
+    './*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
